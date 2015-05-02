4d2b52ea199daad6bc72bb83fe61cf4ff019704c
akmal <akmal@brighter.com>
2015-05-02 02:02:39 -0700

    debug logging for local strategy

--- auth/strategy/local.js
+++ auth/strategy/local.js
@@ -6,8 +6,11 @@ module.exports = function (username, password, done) {
     logger.info("username: ", username, " and password is ", password);
 
     UserModel.findOne({username : username}, function(err, user){
-        logger.debug(err, user);
-        if(err) return done(err, null);
+        if(err) {
+            logger.debug("error finding user ", err);
+            return done(err, null);
+        }
+
         if(!user) return done(null,null, "user not found");
         if(user.password == password) { //authentication successful
             done(null, user);
