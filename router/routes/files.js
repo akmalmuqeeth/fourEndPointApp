2c0fa79c80015745fca5d9541364934cd4e3b2a2
akmal <akmal@brighter.com>
2015-05-02 02:06:30 -0700

    logging for files api

--- router/routes/files.js
+++ router/routes/files.js
@@ -8,6 +8,7 @@ router.get('/:directory', function getFiles(req,res){
     var location = path.join(__dirname, '../../' + req.params.directory);
     fs.readdir(location, function (err, files) {
         if (err) {
+            logger.debug("error while reading directory " +location+ ". Error: "+  err);
             throw err;
         }
         return res.json({"path" : location, "files" : files});
