a8b16b12c60e28e68ef4b8370e3a01c1e6a94ec0
akmal <akmal@brighter.com>
2015-05-03 19:27:02 -0700

    mocha tests

---
+++ test/userTest.js
@@ -0,0 +1,35 @@
+var request = require('supertest')
+    , https = require('https')
+    , fs = require('fs')
+    , path = require('path')
+    , should = require('should')
+    , express = require('express')
+    , router = require('../router');
+
+describe('status(url)', function() {
+    it('should return the status of the application', function (done) {
+        var app = express();
+        router(app);
+
+        var s = app.listen(function () {
+            var url = 'http://localhost:' + s.address().port + "/api/status";
+            request(url)
+                .get('/')
+                .expect('{"mongoDB":"connecting"}', done);
+        });
+    })
+});
+
+describe('files(url)', function() {
+    it('should return the status of the application', function (done) {
+        var app = express();
+        router(app);
+
+        var s = app.listen(function () {
+            var url = 'http://localhost:' + s.address().port + "/api/files/middlewares";
+            request(url)
+                .get('/')
+                .expect('{"mongoDB":"connecting"}', done);
+        });
+    })
+});
